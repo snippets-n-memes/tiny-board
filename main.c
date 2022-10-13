74c544c3b6eca012601b1a74bf274b1e26684176
Mike McMullen <mbmcmullen27@gmail.com>
2022-10-13 15:56:34 -0500

    ticket selection mock

--- main.c
+++ main.c
@@ -24,13 +24,24 @@ void run(){
   for(int i = 0; i < 4; i++) menu[i] = newMenu(i*WWIDTH);
 
   Ticket test1 = *newTicket(menu[0],"testing", "test tickets with a long description.");
+  Ticket test2 = *newTicket(menu[0],"test chain", "test tickets adding an addtional ticket, with a long description.");
+  Ticket test3 = *newTicket(menu[0],"test chain", "overwrite... adding an addtional ticket, with a long description.");
+  Ticket test4 = *newTicket(menu[1],"test chain", "make decision about managing tickets as files, or by writing driver code in go and manage in memory");
+
 
   for(int i = 0; i < 4; i++) {
     drawMenu(menu[i], title[i]);
     wgetch(menu[i]);
   }
 
-  drawTicket(&test1);
+  
+  test1.pos = drawTicket(&test1, 4);
+  test2.pos = drawTicket(&test2, test1.pos+2);
+  test3.pos = drawTicket(&test3, test1.pos+2);
+  test4.pos = drawTicket(&test4, 4);
+  selectTicket(&test4);
+  selectTicket(&test3);
+  selectTicket(&test1);
 
   endwin();
 }
@@ -39,15 +50,22 @@ WINDOW *newMenu(int x){
   return newwin(LINES,WWIDTH,0,x);
 }
 
-Ticket* newTicket(WINDOW *menu, char *title, char *desc) {
+Ticket *newTicket(WINDOW *menu, char *title, char *desc) {
   Ticket* res = malloc(sizeof(Ticket));
   res->name=title;
   res->description=desc;
   res->menu=menu;
+  res->pos=3;
+  res->size = 0;
+  int len = strlen(desc);
+  for (int i = 0; i < len; i += WWIDTH - 6) {
+    res->size++;
+  }
   return res;
 }
 
-void drawMenu(WINDOW * menu, char* title) {
+
+void drawMenu(WINDOW *menu, char* title) {
   //border
   box(menu,0,0);
   wmove(menu,1,1);
@@ -63,24 +81,63 @@ void drawMenu(WINDOW * menu, char* title) {
   wrefresh(menu);
 }
 
-void drawTicket(Ticket *ticket){
+int drawTicket(Ticket *ticket, int line){
   Ticket t = *ticket;
-  int len = strlen(t.description);
-  int y = 3;
+  int y = line;
   int menuWidth = WWIDTH - 6;
+  int len = strlen(t.description);
 
   wmove(t.menu,y,2);
   waddstr(t.menu,t.name);
   wmove(t.menu,++y,2);
   whline(t.menu, ACS_HLINE, WWIDTH - 5);
 
-  for (int i = 0; i < len; i += WWIDTH - 6) {
+  for (int i = 0; i < len; i+= menuWidth) {
+    wmove(t.menu,++y,3);
+    if ((t.description + i)[0] == ' ') i++;
+    wprintw(t.menu,"%.*s", menuWidth, t.description + i);
+  }
+
+  wrefresh(t.menu);
+  wgetch(t.menu);
+  return y + 2;
+}
+
+void selectTicket(Ticket *ticket) {
+  Ticket t = *ticket;
+  int y = t.pos-t.size-4;
+  use_default_colors();
+  start_color();
+  init_pair(1,COLOR_GREEN,-1);
+  wattrset(t.menu, COLOR_PAIR(1));
+  wmove(t.menu, y, 1);
+  waddch(t.menu, ACS_ULCORNER);
+  whline(t.menu, ACS_HLINE, WWIDTH - 4);
+  wmove(t.menu, y, WWIDTH - 2);
+  waddch(t.menu, ACS_URCORNER);
+
+  wmove(t.menu, ++y, WWIDTH - 2);
+  wvline(t.menu, ACS_VLINE, t.size+4);
+  wmove(t.menu, y, 1);
+  wvline(t.menu, ACS_VLINE, t.size+4);
+
+  wmove(t.menu, y+t.size+3, 1);
+  waddch(t.menu, ACS_LLCORNER);
+  whline(t.menu, ACS_HLINE, WWIDTH - 4);
+  wmove(t.menu, y+t.size+3, WWIDTH - 2);
+  waddch(t.menu, ACS_LRCORNER);
+  wattrset(t.menu, A_NORMAL);
+
+  wrefresh(t.menu);
+  wgetch(t.menu);
+}
+
+void clearTicketDesc(Ticket *ticket) {
+  Ticket t = *ticket;
+  int y = t.pos-t.size-2;
+  for(int i=0; i < t.size; i++) {
     wmove(t.menu,++y,2);
-    if (!i){
-      wprintw(t.menu,"- %.*s", menuWidth, t.description + i);
-    } else {
-      wprintw(t.menu,"%.*s", menuWidth, t.description + i);
-    }
+    whline(t.menu, ' ', WWIDTH - 4);
   }
 
   wrefresh(t.menu);
--- main.h
+++ main.h
@@ -8,6 +8,8 @@ typedef enum {unassigned, inprogress, blocked, completed} Windows;
 typedef struct {
   char *name;
   char *description; 
+  int size;
+  int pos;
   WINDOW *menu;
 } Ticket;
 
@@ -15,4 +17,6 @@ void run();
 WINDOW *newMenu(int x);
 Ticket *newTicket(WINDOW *menu, char *title, char *desc);
 void drawMenu(WINDOW *menu, char* title);
-void drawTicket(Ticket *);
+int drawTicket(Ticket *, int lineNumber);
+void selectTicket(Ticket *);
+void clearTicketDesc(Ticket *ticket);

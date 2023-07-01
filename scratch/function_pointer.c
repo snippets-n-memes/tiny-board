f53e00d200ce7676d4f4f8bd2424fb8d9d7f7008
mbmcmullen27 <mbmcmullen27@gmail.com>
2023-07-01 16:29:58 -0500

    test function pointer array

--- scratch/function_pointer.c
+++ scratch/function_pointer.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 void up(int a) {
@@ -19,10 +20,15 @@ void left(int a) {
 
 typedef void (*move)(int);
 typedef struct Test Test;
+typedef struct TestArray TestArray;
 
 struct Test{
   move pressUp;
   move pressDown;
+};
+
+struct TestArray{
+  move *moves;
 };
   
 int main() {
@@ -31,11 +37,25 @@ int main() {
   void (*down_ptr)(int) = &down;
   unit->pressDown = down_ptr;
 
+  TestArray *bigUnit = malloc(sizeof(TestArray));
+  bigUnit->moves = malloc(4*sizeof(move));
 
+  bigUnit->moves[0] = &up;
+  bigUnit->moves[1] = &down;
+  bigUnit->moves[2] = &left;
+  bigUnit->moves[3] = &right;
+
+  printf("\n------ Small Unit test: ------\n");
   (*unit->pressUp)(10);
   (*unit->pressDown)(10);
 
   unit->pressUp = &left;
   (*unit->pressUp)(150);
+
+  printf("\n------ Big Unit test: ------\n");
+  srand(time(NULL));
+  for(int i=0; i<4; i++){
+    (*bigUnit->moves[i])(rand()%10);
+  }
   return 0;
 }
